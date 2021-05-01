14d93c7774e0776b251ad5f4696bc5fecca75190
PrzeZ <przemyslawzykubek@gmail.com>
2021-05-01 20:54:10 +0200

    Work in Progress

--- Lab1/src/mylib/Account.java
+++ Lab1/src/mylib/Account.java
@@ -1,13 +1,28 @@
 package mylib;
 
+import org.apache.commons.lang3;
+
+
 public class Account {
     private String name;
 
     public void setName(String name) {
-        this.name = name;
+        String[] tokens = StringUtils.split(name);
+        for (int i = 0; i < tokens.length; i++)
+            tokens[i] = StringUtils.capitalize(tokens[i]);
+        this.name = StringUtils.join(tokens, ' ');
+
     }
 
     public String getName() {
         return name;
     }
+
+    public static String capitalize(String aStr) {
+        String[] tokens = StringUtils.split(aStr);
+        for (int i = 0; i < tokens.length; i++)
+            tokens[i] = StringUtils.capitalize(tokens[i]);
+        return StringUtils.join(tokens, ' ');
+    }
+
 }
