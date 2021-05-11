81815543d39bd5470934b33e18a700f9cdebdd9c
PrzeZ <56115183+PrzeZ@users.noreply.github.com>
2021-05-11 21:13:01 +0200

    transliteration

--- Lab1/src/mylib/Account.java
+++ Lab1/src/mylib/Account.java
@@ -6,10 +6,12 @@ import org.apache.commons.lang3.*;
 public class Account {
     private String name;
 
-    private static String ukrAlphabet = "абвгдеєжзиіїйклмнопрстуфхцчшщьюя";
+    private static String ukrAlphabet[] = {"а", "б", "в", "г", "д", "е", "є", "ж", "з", "и", "і", "ї", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у"
+            , "ф", "х", "ц", "ч", "ш", "щ", "ь", "ю", "я"};
     private static String translitRepl[] = {"a", "b", "v", "g", "d", "e", "je", "zh", "z", "y", "i", "ji", "j",
             "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "h", "c",
             "ch", "sh", "shh", "'", "ju", "ja"};
+    private static int index = 0;
 
     public void setName(String name) {
         String[] tokens = StringUtils.split(name);
@@ -35,9 +37,12 @@ public class Account {
         String[] tokens = StringUtils.split(arg);
         for (int i = 0; i < tokens.length; i++) {
             tokens[i] = tokens[i].toLowerCase();
-            char ch = arg.charAt(i);
-            int index = StringUtils.indexOf(ch);
-            tokens[i] = ukrAlphabet.charAt(i);
+
+            index = ArrayUtils.indexOf(ukrAlphabet, tokens[i]);
+            if (index < 32 && index < 0) {
+                tokens[i] = translitRepl[index];
+            }
+
         }
 
         return StringUtils.join(tokens, ' ');
