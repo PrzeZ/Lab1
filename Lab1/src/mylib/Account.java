1dcc33374008799e2c7bff5715895d4e784c73fd
PrzeZ <56115183+PrzeZ@users.noreply.github.com>
2021-05-12 22:06:25 +0200

    Fixed translit

--- Lab1/src/mylib/Account.java
+++ Lab1/src/mylib/Account.java
@@ -6,19 +6,11 @@ import org.apache.commons.lang3.*;
 public class Account {
     private String name;
 
-    private static String ukrAlphabet[] = {"а", "б", "в", "г", "д", "е", "є", "ж", "з", "и", "і", "ї", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у"
-            , "ф", "х", "ц", "ч", "ш", "щ", "ь", "ю", "я"};
-    private static String translitRepl[] = {"a", "b", "v", "g", "d", "e", "je", "zh", "z", "y", "i", "ji", "j",
-            "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "h", "c",
-            "ch", "sh", "shh", "'", "ju", "ja"};
-    private static int index = 0;
-
     public void setName(String name) {
         String[] tokens = StringUtils.split(name);
         for (int i = 0; i < tokens.length; i++)
             tokens[i] = StringUtils.capitalize(tokens[i]);
         this.name = StringUtils.join(tokens, ' ');
-
     }
 
     public String getName() {
@@ -32,21 +24,31 @@ public class Account {
         return StringUtils.join(tokens, ' ');
     }
 
-    public static String translit(String arg) {
+    /*public static String translit(String arg) {
 
-        String[] tokens = StringUtils.split(arg);
-        for (int i = 0; i < tokens.length; i++) {
+        String[] tokens = StringUtils.split(arg); <--- caused a lot of trouble :/
+
+        for (int i = 0; i < tokens.length - 1; i++) {
             tokens[i] = tokens[i].toLowerCase();
 
-            index = ArrayUtils.indexOf(ukrAlphabet, tokens[i]);
-            if (index < 32 && index < 0) {
-                tokens[i] = translitRepl[index];
-            }
-
+            int index = ArrayUtils.indexOf(ukrAlphabet, tokens[i]);
+            tokens[i] = translitRepl[index];
         }
 
-        return StringUtils.join(tokens, ' ');
+        return StringUtils.join(tokens, ' ');*/
 
+    public static String translit(String arg) {
+        char[] ukrAlphabet = {' ', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        String[] translitRepl = {" ", "a", "b", "v", "g", "d", "e", "e", "zh", "z", "i", "y", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "h", "ts", "ch", "sh", "sch", "", "i", "", "e", "ju", "ja", "A", "B", "V", "G", "D", "E", "E", "Zh", "Z", "I", "Y", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "F", "H", "Ts", "Ch", "Sh", "Sch", "", "I", "", "E", "Ju", "Ja", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < arg.length(); i++) {
+            for (int j = 0; j < ukrAlphabet.length; j++) {
+                if (arg.charAt(i) == ukrAlphabet[j]) {
+                    builder.append(translitRepl[j]);
+                }
+            }
+        }
+        return builder.toString();
     }
 
 }
--- Lab1/src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ Lab1/src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,15 +2,37 @@ package pl.lublin.wsei.java.cwiczenia;
 
 import mylib.Account;
 
-import java.util.Scanner;
+import java.util.Random;
+
 
 public class Main {
 
+
     public static void main(String[] args) {
+        final int[] liczby = new int[30];
+        Random rnd = new Random();
+        for (int i = 0; i < 30; i++) {
+            liczby[i] = rnd.nextInt();
+        }
+        int mx = Integer.MAX_VALUE;
+        int mn = Integer.MIN_VALUE;
+
+        long avg = 0;
+
+        for (int l : liczby) {
+            System.out.println(l);
+            if (l < mn) mn = l;
+            if (l > mx) mx = l;
+            avg += l;
+        }
+        System.out.printf("min = %d, max = % d, avg = %f", mn, mx, (float) avg / liczby.length);
+
+        System.out.print("\n" + "\n");
+
         Account acc = new Account();
         acc.setName("piotr gołabek");
         System.out.println(acc.getName());
         System.out.printf("%s%n", Account.capitalize("stanisŁaw maruSARz"));
-        System.out.printf(Account.translit("Юлия"));
+        System.out.printf(Account.translit("иван"));
     }
 }
